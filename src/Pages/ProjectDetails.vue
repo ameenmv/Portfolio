d507ce57df93eb7b21f28717a02e9f99280eb37b
Ameen <150484349+ameenmv@users.noreply.github.com>
2025-10-14 21:10:15 +0300

    Update ProjectDetails.vue

--- src/Pages/ProjectDetails.vue
+++ src/Pages/ProjectDetails.vue
@@ -86,7 +86,7 @@
 </template>
 
 <script>
-// imports
+
 import Navbar from "../components/Navbar.vue";
 import Contact from "../components/Contact.vue";
 import Footer from "../components/Footer.vue";
