8f61098327bc81be2a3949a783b4b117b6e7c263
Ameen <shehaaa4@gmail.com>
2025-11-08 18:19:29 +0200

    update cv

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
     class="sm:pl-10 sm:pr-10 pl-4 pr-4 flex justify-between items-center absolute top-0 left-0 right-0 h-[80px] z-50 h-[8vh]"
   >
     <router-link to="/"
-      ><img class="w-[50px]" src="../assets/mv.png" alt=""
+      ><img class="w-1/2" src="../assets/mv.png" alt=""
     /></router-link>
     <div class="flex gap-2 items-center sm:gap-6">
       <router-link to="/projects"
@@ -14,7 +14,7 @@
         </p></router-link
       >
       <a
-        href="https://drive.google.com/uc?export=download&id=1m6MF1BJyxhEAiT4FDg9s4VQtQaUOwY71"
+        href="https://drive.google.com/uc?export=download&id=1ka64aGMrpVLL2XMh7FFTvvwckzTFXyij"
         ><div class="btnn ml-5 relative">
           <div class="btn">my cv ?</div>
           <div class="btn2"></div>
